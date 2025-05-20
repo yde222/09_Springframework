7bcedd7ff6d28e7808a2bfada817a6aa3449fbac
heechae <chaehee15@naver.com>
2025-05-20 15:02:24 +0900

    0520 servlet 수업파일1

---
+++ 14_LEECHAEHEE/servlet-lecture/chap01-life-cycle-lecture-source/src/main/java/com/ohgiraffers/chap01lifecyclelecturesource/section02/annotation/LifeCycleTest.java
@@ -0,0 +1,36 @@
+package com.ohgiraffers.chap01lifecyclelecturesource.section02.annotation;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+/*
+* @WebServlet : 서블릿을 서블릿 컨테이너에 등록
+* value 속성 : 서블릿 매핑
+* */
+
+@WebServlet(value="/annotation-lifecycle", loadOnStartup=1)
+public class LifeCycleTest extends HttpServlet {
+
+    public LifeCycleTest() {
+        System.out.println("Annotation 생성자 호출");
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("Annotation Init 메소드 호출");
+
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        System.out.println("Annotation Mapping Service 메소드 호출");
+
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Annotation Mapping Destroy 메소드 호출");
+    }
+}
---
+++ 14_LEECHAEHEE/servlet-lecture/chap03-response-lecture-source/src/main/java/com/ohgiraffers/chap03responselecturesource/section02/headers/ResponseHeaderPrint.java
@@ -0,0 +1,27 @@
+package com.ohgiraffers.chap03responselecturesource.section02.headers;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+@WebServlet(value = "/headers")
+public class ResponseHeaderPrint extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        long currentTime = System.currentTimeMillis();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<html>")
+                .append("<head>")
+                .append("<title>응답페이지</title>")
+                .append("</head>")
+                .append("<body>")
+                .append("<h1>")
+                .append(currentTime)
+                .append("</h1>")
+                .append("</body>")
+                .append("</html>");
+    }
+}
