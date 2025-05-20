94c751485f9d360cab78501f5047cd8013860140
junseo <junseo9145@gmail.com>
2025-05-20 17:47:04 +0900

    20250520 수업 종료 커밋

--- 05_PARKJUNSEO/servlet-lecture/chap04-forward-redirect-lecture-source/src/main/java/com/ohgiraffers/chap04forwardredirectlecturesource/section02/redirect/Redirect.java
+++ 05_PARKJUNSEO/servlet-lecture/chap04-forward-redirect-lecture-source/src/main/java/com/ohgiraffers/chap04forwardredirectlecturesource/section02/redirect/Redirect.java
@@ -9,18 +9,17 @@ import java.io.PrintWriter;
 
 @WebServlet(value = "/redirect")
 public class Redirect extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("이 서블릿으로 redirect 완료!");
-        request.getAttribute("name2");
-        Object name2 = (String)request.getAttribute("name2");
+        System.out.println("이 서블릿으로 redirect완료!");
+        String name2 = (String)request.getAttribute("name2");
         System.out.println("name2 = " + name2);
 
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>이 서블릿으로 redirect 완료!</h1>");
+        out.println("<h1>이 서블릿으로 redirect완료!</h1>");
         out.flush();
         out.close();
     }
-
 }
