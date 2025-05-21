e5f50f0e4a79895557ecb6f61ef767922a1fd1ee
macacolabs <monkey.ohgiraffers@gmail.com>
2025-05-21 15:50:15 +0900

    commit

--- 00_YUSEUNGJE/servlet-lecture/chap07-listener-lecture-source/src/main/java/com/ohgiraffers/chap07listenerlecturesource/section03/requestlistener/RequestListenerTestServlet.java
+++ 00_YUSEUNGJE/servlet-lecture/chap07-listener-lecture-source/src/main/java/com/ohgiraffers/chap07listenerlecturesource/section03/requestlistener/RequestListenerTestServlet.java
@@ -10,6 +10,13 @@ import java.io.IOException;
 public class RequestListenerTestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("username", "honggildong");
+        request.setAttribute("age", 20);
+        request.setAttribute("gender", "M");
+
+        request.setAttribute("username", "hong");
+
+        request.removeAttribute("gender");
 
     }
 }
