0283897812b8b266556c64b289a7981237fd73ff
macacolabs <monkey.ohgiraffers@gmail.com>
2025-05-21 12:50:41 +0900

    listener upload

--- 00_YUSEUNGJE/servlet-lecture/chap06-filter-lecture-source/src/main/java/com/ohgiraffers/chap06filterlecturesource/section02/uses/RegistmemberServlet.java
+++ 00_YUSEUNGJE/servlet-lecture/chap06-filter-lecture-source/src/main/java/com/ohgiraffers/chap06filterlecturesource/section02/uses/RegistmemberServlet.java
@@ -3,6 +3,7 @@ package com.ohgiraffers.chap06filterlecturesource.section02.uses;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.io.IOException;
 
@@ -17,5 +18,12 @@ public class RegistmemberServlet extends HttpServlet {
         System.out.println("userId = " + userId);
         System.out.println("password = " + password);
         System.out.println("name = " + name);
+
+        /*
+        * 암호화 된 패스워드는 동일 값이 입력되더라도 매번 실행 시 salt에 의해 다른 값을
+        * 가지게 된다. 입력 값 동일 여부 확인은 matches라는 메소드를 통해 확인한다.
+        * */
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println("비밀번호가 pass01? " + encoder.matches("pass01", password));
     }
 }
