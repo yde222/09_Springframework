3b9845472a7363165f15d6fb341b14060c2f4a0a
macacolabs <monkey.ohgiraffers@gmail.com>
2025-05-22 16:23:15 +0900

    commit

---
+++ 00_YUSEUNGJE/spring-lecture/01_spring-core/chap03-di-annotation/src/main/java/com/ohgiraffers/section01/autowired/subsection01/field/Application.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.section01.autowired.subsection01.field;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.ohgiraffers.section01");
+        String[] definitionNames = context.getBeanDefinitionNames();
+        for (String definitionName : definitionNames) {
+            System.out.println("definitionName = " + definitionName);
+        }
+    }
+}
