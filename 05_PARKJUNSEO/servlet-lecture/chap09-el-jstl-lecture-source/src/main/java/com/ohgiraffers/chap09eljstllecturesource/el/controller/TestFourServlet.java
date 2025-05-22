82f031e7958d28421cca0595afdef932192e1f6e
junseo <junseo9145@gmail.com>
2025-05-22 17:28:14 +0900

    20250522 수업 종료 커밋

--- 05_PARKJUNSEO/servlet-lecture/chap09-el-jstl-lecture-source/src/main/java/com/ohgiraffers/chap09eljstllecturesource/el/controller/TestFourServlet.java
+++ 05_PARKJUNSEO/servlet-lecture/chap09-el-jstl-lecture-source/src/main/java/com/ohgiraffers/chap09eljstllecturesource/el/controller/TestFourServlet.java
@@ -7,6 +7,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
@@ -14,14 +15,14 @@ import java.io.IOException;
 public class TestFourServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        MemberDTO sessionMember = new MemberDTO("홍창기", 19, "010-1234-5678", "gildong@ohgiraffers.com");
-        MemberDTO requestMember = new MemberDTO("유영찬    ", 19, "010-1234-5678", "gildong@ohgiraffers.com");
+        MemberDTO sesstionMember = new MemberDTO("홍길동", 19, "010-1234-5678", "gildong@ohgiraffers.com");
+        MemberDTO requestMember = new MemberDTO("유관순", 14, "010-1234-5578", "yu@ohgiraffers.com");
 
-        request.setAttribute("member", sessionMember);
+        HttpSession session = request.getSession();
+        session.setAttribute("member", sesstionMember);
         request.setAttribute("member", requestMember);
 
         RequestDispatcher rd = request.getRequestDispatcher("views/el/testEl4.jsp");
         rd.forward(request, response);
     }
-
 }
