ba3bf0ce96519b263401903a5a62b00e86d47503
jungho <kiik52@naver.com>
2025-05-23 10:26:38 +0900

    chap03 - di-annotation - resource (자바 진영에서 제공하는 DI Annotation)

---
+++ 20_JIJUNGHO/spring-lecture/01_spring-core/chap03-di-annotation/src/main/java/com/ohgiraffers/section02/annotation/subsection04/resource/PokemonService.java
@@ -0,0 +1,23 @@
+package com.ohgiraffers.section02.annotation.subsection04.resource;
+
+import com.ohgiraffers.section02.annotation.common.Pokemon;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+@Service("pokemonServiceResource")
+public class PokemonService {
+    /*
+    * @Resource : 자바 진영에서 제공하는 DI Annotation
+    * @AutoWired와 달리 name 속성 값으로 의존성 주입을 할 수 있고
+    * 필드 주입, 세터 주입이 가능하다.
+    * 만약 List<Pokemon> 타입이라면 name 속성을 생략하면 3개의 bean이 담긴다.
+    * (이름 -> 타입 순서로 처리)
+    * */
+
+    @Resource(name="squiretle")
+    private Pokemon pokemon;
+
+    public void pokemonAttack() {
+        pokemon.attack();
+    }
+}
