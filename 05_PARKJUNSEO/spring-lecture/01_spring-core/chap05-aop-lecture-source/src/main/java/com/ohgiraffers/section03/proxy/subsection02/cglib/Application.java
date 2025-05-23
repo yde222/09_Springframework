db199933b5f5cd4c59d06a1cfb8de6d63c9652d3
junseo <junseo9145@gmail.com>
2025-05-23 17:48:22 +0900

    20250523 수업 종료 커밋

---
+++ 05_PARKJUNSEO/spring-lecture/01_spring-core/chap05-aop-lecture-source/src/main/java/com/ohgiraffers/section03/proxy/subsection02/cglib/Application.java
@@ -0,0 +1,21 @@
+package com.ohgiraffers.section03.proxy.subsection02.cglib;
+
+import com.ohgiraffers.section03.proxy.common.OhgiraffersStudent;
+import org.springframework.cglib.proxy.Enhancer;
+
+public class Application {
+    public static void main(String[] args) {
+
+        /*
+        * 2. CGLib 방식
+        * Target Object의 타입이 Class여도 가능하다.
+        * */
+
+        OhgiraffersStudent ohgiraffersStudent = new OhgiraffersStudent(); // 여기서 OhgiraffersStudent는 클래스
+        Handler handler = new Handler(ohgiraffersStudent);
+
+        OhgiraffersStudent proxy = (OhgiraffersStudent) Enhancer.create(OhgiraffersStudent.class, handler);
+
+        proxy.study(16);
+    }
+}
