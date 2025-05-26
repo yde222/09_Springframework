ddc18e83bfe2f935a63ffb89e1d6f46ca10f839d
parkchangjun <ory01@hs.ac.kr>
2025-05-26 10:51:41 +0900

    05/22 classmapping source commit

---
+++ 06_PARKCHANGJUN/spring-lecture/02_spring-web/chap01-request-mapping-lecture-source/src/main/java/com/ohgiraffers/requestmapping/ClassMappingTestController.java
@@ -0,0 +1,11 @@
+package com.ohgiraffers.requestmapping;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/order")
+public class ClassMappingTestController {
+
+    
+}
