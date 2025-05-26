5cd14b15a25602be0c072d2761ea6dc9db442fcd
juhui <juya202106@naver.com>
2025-05-26 17:05:16 +0900

    view resolver, exception handelr, interceptor

--- 12_JUHUI/spring-web/chap04-exception-handler-lecture-source/src/main/java/com/ohgiraffers/exceptionhandler/ExceptionHandlerController.java
+++ 12_JUHUI/spring-web/chap04-exception-handler-lecture-source/src/main/java/com/ohgiraffers/exceptionhandler/ExceptionHandlerController.java
@@ -1,4 +1,29 @@
 package com.ohgiraffers.exceptionhandler;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ExceptionHandlerController {
+    @GetMapping("/controller-null")
+    public String nullPointerException() {
+        String str = null;
+        System.out.println(str.charAt(0)); // 의도적으로 NullPointerException 발생
+        return "/";
+    }
+
+    @GetMapping("/controller-user")
+    public String userExceptionTest() throws MemberRegistException {
+        boolean check = true;
+        if(check) throw new MemberRegistException("당신 같은 사람은 회원으로 받을 수 없습니다.");
+        return "/";
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public String nullPointerExcptionHandler(NullPointerException e){
+        System.out.println("지역 범위의 Exception Handler 동작");
+        System.out.println("message: " + e.getMessage());
+        return "error/nullPointer";
+    }
 }
--- 12_JUHUI/spring-web/chap04-exception-handler-lecture-source/src/main/java/com/ohgiraffers/exceptionhandler/MemberRegistException.java
+++ 12_JUHUI/spring-web/chap04-exception-handler-lecture-source/src/main/java/com/ohgiraffers/exceptionhandler/MemberRegistException.java
@@ -1,4 +1,8 @@
 package com.ohgiraffers.exceptionhandler;
 
-public class MemberRegistException {
+/* 사용자 정의 Exception 클래스 */
+public class MemberRegistException extends Exception {
+    public MemberRegistException(String message) {
+        super(message);
+    }
 }
