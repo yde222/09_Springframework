a384370d1baf0b2b4c4a5f9235622a1a50b62f6d
junseo <junseo9145@gmail.com>
2025-05-26 17:13:57 +0900

    20250526 수업 종료 커밋

---
+++ 05_PARKJUNSEO/spring-lecture/02_spring-web/chap05-interceptor-lecture-source/src/main/java/com/ohgiraffers/interceptor/Chap05InterceptorLectureSourceApplication.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.interceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap05InterceptorLectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap05InterceptorLectureSourceApplication.class, args);
+    }
+}
