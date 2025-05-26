9142b93c7d338239a636f6453c940e5d8b842876
juhui <juya202106@naver.com>
2025-05-26 17:28:08 +0900

    request mapping 오류해결

--- 12_JUHUI/spring-web/chap01-request-mapping-lecture-source/src/main/java/com/ohgiraffers/requestmapping/MethodMappingTestController.java
+++ 12_JUHUI/spring-web/chap01-request-mapping-lecture-source/src/main/java/com/ohgiraffers/requestmapping/MethodMappingTestController.java
@@ -36,24 +36,4 @@ public class MethodMappingTestController {
         model.addAttribute("message", "POST 방식의 삭제 핸들러 메소드 호출");
         return "mappingResult";
     }
-
-    /* 여러 주소 값과 매핑 */
-    @RequestMapping(value = {"/modify", "/delete"}, method = RequestMethod.POST)
-    public String modifyAndDeleteOrder(Model model) {
-        model.addAttribute("message", "POST 방식의 주문 수정, 삭제 핸들러 메소드 호출");
-        return "mappingResult";
-    }
-
-    /* Path Variable : 요청 주소에 포함된 변수 */
-    @GetMapping("/detail/{orderNo}")
-    public String detailOrder(@PathVariable("orderNo") int orderNo, Model model) {
-        model.addAttribute("message", "GET 방식의 " + orderNo + "번 주문 상세 핸들러 메소드 호출");
-        return "mappingResult";
-    }
-
-    @RequestMapping()
-    public String otherRequest(Model model) {
-        model.addAttribute("message", "order 요청이긴 하지만 다른 기능은 아직 준비 되지않음");
-        return "mappingResult";
-    }
 }
