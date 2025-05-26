dcae379a19ab134bc32dbc5fbdbc7fab327aa64f
macacolabs <monkey.ohgiraffers@gmail.com>
2025-05-26 09:51:20 +0900

    request Mapping

---
+++ 00_YUSEUNGJE/spring-lecture/02_spring-web/chap01-request-mapping-lecture-source/src/main/java/com/ohgiraffers/requestmapping/MethodMappingTestController.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.requestmapping;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller   // 빈으로 등록
+public class MethodMappingTestController {
+
+    /* 1. http method방식을 미지정*/
+}
