7e8655205864d6bbaf58be12717ba999094db549
dkrio <dkrio34@gmail.com>
2025-05-27 11:50:51 +0900

    수업자료 최신화

---
+++ 18_JUNGSEONGWON/spring-lecture/02_spring-web/chap07-thymeleaf-lecture-source/src/test/java/com/jsw/thymeleaf/Chap07ThymeleafLectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsw.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap07ThymeleafLectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
