f3f1156e034f36e2bf8dd719a80ce5d4cd8ec173
FerryLa <rlawl4240@gmail.com>
2025-05-28 08:59:39 +0900

    250527, 250528포크

---
+++ 01_KIMJIHWAN/mybatis-lecture/chap02-crud-lecture-source/src/main/java/com/ohgiraffers/section01/xmlconfig/MenuController.java
@@ -0,0 +1,40 @@
+package com.ohgiraffers.section01.xmlconfig;
+
+import java.util.List;
+import java.util.Map;
+
+public class MenuController {
+
+    private final MenuService menuService;
+    private final PrintResult printResult;
+
+    public MenuController() {
+        this.menuService = new MenuService();
+        this.printResult = new PrintResult();
+    }
+
+    public void selectAllMenu() {
+        List<MenuDTO> menuList = menuService.selectAllMenu();
+
+        if(menuList != null && !menuList.isEmpty()){
+            printResult.printMenuList(menuList);
+        } else {
+            printResult.printErrorMessage("selectList");
+        }
+    }
+
+    public void selectMenuByMenuCode(Map<String, String> stringStringMap) {
+
+        int menuCode = Integer.parseInt(stringStringMap.get("menuCode"));
+
+        MenuDTO menu = menuService.selectMenuByMenuCode(menuCode);
+
+        if(menu != null){
+            printResult.printMenu(menu);
+        } else {
+            printResult.printErrorMessage("selectOne");
+        }
+
+
+    }
+}
---
+++ 01_KIMJIHWAN/spring-lecture/02_spring-web/chap04-exception-handler-leture-source/src/main/java/com/ohgiraffers/exceptionhandler/MemberRegistException.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.exceptionhandler;
+
+/* 사용자 정의 Exception 클래스 */
+public class MemberRegistException extends Exception {
+
+
+    public MemberRegistException(String message) {
+        super(message);
+    }
+}
