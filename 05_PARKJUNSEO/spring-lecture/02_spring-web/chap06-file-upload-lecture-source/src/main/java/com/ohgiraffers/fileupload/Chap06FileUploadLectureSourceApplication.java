6c93e61eb3500a96acd8744eeb5bbbf5fd353e79
junseo <junseo9145@gmail.com>
2025-05-27 11:48:58 +0900

    수업 중간 커밋

--- 05_PARKJUNSEO/spring-lecture/02_spring-web/chap06-file-upload-lecture-source/src/main/java/com/ohgiraffers/fileupload/Chap06FileUploadLectureSourceApplication.java
+++ 05_PARKJUNSEO/spring-lecture/02_spring-web/chap06-file-upload-lecture-source/src/main/java/com/ohgiraffers/fileupload/Chap06FileUploadLectureSourceApplication.java
@@ -1,13 +1,13 @@
-package com.ohgiraffers.interceptor;
+package com.ohgiraffers.fileupload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Chap05InterceptorLectureSourceApplication {
+public class Chap06FileUploadLectureSourceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Chap05InterceptorLectureSourceApplication.class, args);
+        SpringApplication.run(Chap06FileUploadLectureSourceApplication.class, args);
     }
+
 }
-    
--- 05_PARKJUNSEO/spring-lecture/02_spring-web/chap07-thymeleaf-lecture-source/src/main/java/com/ohgiraffers/thymeleaf/Chap07ThymeleafLectureSourceApplication.java
+++ 05_PARKJUNSEO/spring-lecture/02_spring-web/chap07-thymeleaf-lecture-source/src/main/java/com/ohgiraffers/thymeleaf/Chap07ThymeleafLectureSourceApplication.java
@@ -1,13 +1,13 @@
-package com.ohgiraffers.interceptor;
+package com.ohgiraffers.thymeleaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Chap05InterceptorLectureSourceApplication {
+public class Chap07ThymeleafLectureSourceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Chap05InterceptorLectureSourceApplication.class, args);
+        SpringApplication.run(Chap07ThymeleafLectureSourceApplication.class, args);
     }
+
 }
-    
