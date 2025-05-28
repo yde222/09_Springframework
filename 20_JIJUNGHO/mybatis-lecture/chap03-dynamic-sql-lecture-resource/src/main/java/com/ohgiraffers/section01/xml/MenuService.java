94ff3dfb5495bcff23124a35b1c699025693ac32
jungho <kiik52@naver.com>
2025-05-28 14:30:46 +0900

    chap03- dynamic-sql - if

---
+++ 20_JIJUNGHO/mybatis-lecture/chap03-dynamic-sql-lecture-resource/src/main/java/com/ohgiraffers/section01/xml/MenuService.java
@@ -0,0 +1,52 @@
+package com.ohgiraffers.section01.xml;
+
+import com.ohgiraffers.common.MenuDTO;
+import com.ohgiraffers.common.SearchCriteria;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.ohgiraffers.common.Template.getSqlSession;
+
+public class MenuService {
+
+    public void selectMenuByPrice (int price) {
+
+        SqlSession sqlSession = getSqlSession();
+        DynamicSqlMapper mapper = sqlSession.getMapper(DynamicSqlMapper.class);
+
+        /*
+        * 기본 자료형의 변수명으로는 if문의 조건에서 값을 비교할 수 없다.
+        * Map에 기본 자료형 값을 담아 전달하도록 한다.
+        * (Map의 Key값, DTO객체의 필드값)
+        * */
+
+        Map<String, Integer> map = new HashMap<>();
+        map.put("price", price);
+
+        List<MenuDTO> menuList = mapper.selectMenuByPrice(map);
+
+        if (menuList != null || menuList.isEmpty()) {
+            menuList.forEach(System.out::println);
+        } else {
+            System.out.println("검색 결과가 존재하지 않습니다.");
+        }
+        sqlSession.close();
+    }
+
+    public void selectMenu(SearchCriteria searchCriteria) {
+        SqlSession sqlSession = getSqlSession();
+        DynamicSqlMapper mapper = sqlSession.getMapper(DynamicSqlMapper.class);
+
+        List<MenuDTO> menuList = mapper.selectMenu(searchCriteria);
+
+        if (menuList != null || menuList.isEmpty()) {
+            menuList.forEach(System.out::println);
+        } else {
+            System.out.println("검색 결과가 존재하지 않습니다.");
+        }
+        sqlSession.close();
+    }
+}
