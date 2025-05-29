d09a64eb0579ab18b7c12664aff1698597160bee
junseo <junseo9145@gmail.com>
2025-05-29 11:52:25 +0900

    mybatis 실습 과제 최종 제출

--- 05_PARKJUNSEO/mybatis-lecture/chap99_employee-practice-source/src/main/java/com/ohgiraffers/practice/employee/view/MainView.java
+++ 05_PARKJUNSEO/mybatis-lecture/chap99_employee-practice-source/src/main/java/com/ohgiraffers/practice/employee/view/MainView.java
@@ -13,14 +13,14 @@ public class MainView {
         Scanner sc = new Scanner(System.in);
         EmployeeController ec = new EmployeeController();
         while(true) {
-            System.out.println("===== 회원 관리 ======");
-            System.out.println("1. 회원 전체 조회");
-            System.out.println("2. 회원 코드로 메뉴 조회");
-            System.out.println("3. 회원 추가");
-            System.out.println("4. 회원 수정");
-            System.out.println("5. 회원 삭제");
+            System.out.println("===== 사원 관리 ======");
+            System.out.println("1. 사원 전체 조회");
+            System.out.println("2. 사원 코드로 메뉴 조회");
+            System.out.println("3. 사원 추가");
+            System.out.println("4. 사원 수정");
+            System.out.println("5. 사원 삭제");
             System.out.println("9. 프로그램 종료");
-            System.out.print("회원 관리 번호 입력 : ");
+            System.out.print("사원 관리 번호 입력 : ");
             int no = sc.nextInt();
             switch (no) {
                 case 1:
@@ -50,7 +50,7 @@ public class MainView {
     private static EmployeeDTO inputEmployee() {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("회원번호를 입력하세요: ");
+        System.out.print("사원번호를 입력하세요: ");
         int empId = sc.nextInt();
         sc.nextLine();  // ← 버퍼 비우기
         System.out.print("이름을 입력하세요: ");
@@ -79,7 +79,7 @@ public class MainView {
     private static Map<String, String> inputEmpId() {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("회원 번호 입력 : ");
+        System.out.print("사원 번호 입력 : ");
         String empId = sc.nextLine();
         Map<String, String> parameter = new HashMap<>();
         parameter.put("empId", empId);
@@ -89,7 +89,7 @@ public class MainView {
 
     private static Map<String, String> inputUpdateEmployee() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("수정할 회원 번호를 입력하세요 : ");
+        System.out.print("수정할 사원 번호를 입력하세요 : ");
         String empId = sc.nextLine();
         System.out.print("수정할 이름을 입력하세요: ");
         String empName = sc.nextLine();
