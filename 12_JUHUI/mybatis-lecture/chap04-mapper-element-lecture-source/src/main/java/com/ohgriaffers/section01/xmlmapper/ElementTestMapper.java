69e09c36d1415248c26fc948a2b2609d3f176b3e
juhui <juya202106@naver.com>
2025-05-29 11:51:28 +0900

    11시 수업chap04 생성 후 코드 저장

---
+++ 12_JUHUI/mybatis-lecture/chap04-mapper-element-lecture-source/src/main/java/com/ohgriaffers/section01/xmlmapper/ElementTestMapper.java
@@ -0,0 +1,10 @@
+package com.ohgriaffers.section01.xmlmapper;
+
+import java.util.List;
+
+public interface ElementTestMapper {
+
+    List<MenuDTO> selectResultMapTest();
+
+    List<MenuAndCategoryDTO> selectResultMapAssociationTest();
+}
