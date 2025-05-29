fc9fad6a22c35cb2c33b9424cf388490c09ca1fb
Ji-Jung-Ho <kiik52@naver.com>
2025-05-29 11:38:04 +0900

    chap04 - mapper-element - Mapper Element 테스트   - resultMap

---
+++ 20_JIJUNGHO/mybatis-lecture/chap04-mapper-element-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/ElementTestMapper.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.section01.xmlmapper;
+
+import java.util.List;
+
+public interface ElementTestMapper {
+
+    List<MenuDTO> selectResultMapTest();
+}
---
+++ 20_JIJUNGHO/mybatis-lecture/chap04-mapper-element-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/ElementTestService.java
@@ -0,0 +1,25 @@
+package com.ohgiraffers.section01.xmlmapper;
+
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.List;
+
+import static com.ohgiraffers.common.Template.getSqlSession;
+
+public class ElementTestService {
+
+    public void selectResultMapTest() {
+        SqlSession sqlSession = getSqlSession();
+        ElementTestMapper mapper = sqlSession.getMapper(ElementTestMapper.class);
+
+        List<MenuDTO> menuList = mapper.selectResultMapTest();
+        
+        if (menuList != null && !menuList.isEmpty()) {
+            menuList.forEach(System.out::println);
+        } else {
+            System.out.println("조회 결과 없음");
+        }
+
+    }
+
+}
