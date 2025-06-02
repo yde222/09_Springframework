3021b39d4c0e51743c4b6c575d63afecee43c9b2
macacolabs <monkey.ohgiraffers@gmail.com>
2025-06-02 16:49:52 +0900

    commit

--- 00_YUSEUNGJE/jpa-lecture/chap03-association-mapping-lecture-source/src/test/java/com/ohgiraffers/associationmapping/section03/bidirection/BiDirectionServiceTest.java
+++ 00_YUSEUNGJE/jpa-lecture/chap03-association-mapping-lecture-source/src/test/java/com/ohgiraffers/associationmapping/section03/bidirection/BiDirectionServiceTest.java
@@ -3,8 +3,13 @@ package com.ohgiraffers.associationmapping.section03.bidirection;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -38,4 +43,52 @@ class BiDirectionServiceTest {
         Assertions.assertEquals(categoryCode, foundCategory.getCategoryCode());
        // System.out.println("foundCategory ===>> " + foundCategory);
     }
+
+
+    private static Stream<Arguments> getMenuInfo(){
+        return Stream.of(
+                Arguments.of(111, "스테이크 크림 파스타", 9000, "Y")
+        );
+    }
+
+    @DisplayName("양방향 연관관계 주인 객체를 이용한 삽입 테스트")
+    @ParameterizedTest
+    @MethodSource("getMenuInfo")
+    void biDirectionInsertTest1(int menuCode, String menuName, int menuPrice, String orderableStatus){
+        // given
+        Category category = biDirectionService.findCategory(4);  // 영속화
+        Menu menu = new Menu(menuCode, menuName, menuPrice, category, orderableStatus);
+        // when
+        // then
+        Assertions.assertDoesNotThrow(
+                () -> biDirectionService.registMenu(menu)
+        );
+    }
+
+    private static Stream<Arguments> getCategoryInfo(){
+        return Stream.of(
+                Arguments.of(111, "양방향 카테고리", null)
+        );
+    }
+
+    @DisplayName("양방향 연관관계 주인이 아닌 객체를 이용한 삽입 테스트")
+    @ParameterizedTest
+    @MethodSource("getCategoryInfo")
+    void biDirectionInsertTest2(int categoryCode, String categoryName, Integer refCategoryCode){
+        // given
+        Category category = new Category(categoryCode, categoryName, refCategoryCode);
+        // when
+        biDirectionService.registCategory(category);
+        // then
+        Category foundCategory = biDirectionService.findCategory(categoryCode);
+        Assertions.assertNotNull(foundCategory);
+    }
+
+
+
+
+
+
+
+
 }
