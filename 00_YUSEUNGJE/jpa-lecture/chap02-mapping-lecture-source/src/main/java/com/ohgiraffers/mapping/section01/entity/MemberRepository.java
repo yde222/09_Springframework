a2485e1b85dbf59a0feb5dae5c8a3098a18b4475
macacolabs <monkey.ohgiraffers@gmail.com>
2025-06-02 10:50:52 +0900

    2st period upload

--- 00_YUSEUNGJE/jpa-lecture/chap02-mapping-lecture-source/src/main/java/com/ohgiraffers/mapping/section01/entity/MemberRepository.java
+++ 00_YUSEUNGJE/jpa-lecture/chap02-mapping-lecture-source/src/main/java/com/ohgiraffers/mapping/section01/entity/MemberRepository.java
@@ -13,4 +13,11 @@ public class MemberRepository {
     public void save(Member member) {
         entityManager.persist(member);
     }
+
+    public String findNameById(String memberId) {
+        // JPQL 활용(테이블명 -> 엔티티명, 컬럼명 -> 필드명)
+        // 엔터티는 반드시 별칭을 사용해야 한다.
+        String jpql = "SELECT m.memberName FROM entityMember m WHERE m.memberId='" + memberId + "'";
+        return entityManager.createQuery(jpql, String.class).getSingleResult();
+    }
 }
