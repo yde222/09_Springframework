e40a1a0b8eae9d2c4429cdc4eabb13972aca40eb
jungho <kiik52@naver.com>
2025-06-04 09:24:37 +0900

    chap04 - jpql - 이름을 기준으로 파라미터 바인딩 테스트 - 위치를 기준으로 파라미터 바인딩 테스트

---
+++ 20_JIJUNGHO/jpa-lecture/chap04-jpql-lecture-source/src/test/java/com/ohgiraffers/jpql/section02/ParameterBindRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.ohgiraffers.jpql.section02;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ParameterBindRepositoryTest {
+
+    @Autowired
+    private ParameterBindRepository parameterBindRepository;
+
+    @DisplayName("이름을 기준으로 파라미터 바인딩 테스트")
+    @Test
+    void testSelectMenuByBindingName() {
+        String menuName = "한우딸기국밥";
+        List<Menu> menuList = parameterBindRepository.selectMenuByBindingName(menuName);
+
+        assertNotNull(menuList);
+        assertEquals(menuName, menuList.get(0).getMenuName());
+    }
+
+    @DisplayName("위치를 기준으로 파라미터 바인딩 테스트")
+    @Test
+    void testSelectMenuByBindingPosition() {
+        String menuName = "한우딸기국밥";
+        List<Menu> menuList = parameterBindRepository.selectMenuByBindingPosition(menuName);
+
+        assertNotNull(menuList);
+        assertEquals(menuName, menuList.get(0).getMenuName());
+    }
+
+
+}
