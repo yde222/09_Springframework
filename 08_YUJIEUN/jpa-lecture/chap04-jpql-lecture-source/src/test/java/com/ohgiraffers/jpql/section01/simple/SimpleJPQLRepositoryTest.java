65c5aa6b726cda95191762f4a712aa53bd84b164
jieun <yudaeun222@gmail.com>
2025-06-04 09:26:19 +0900

    add

--- 08_YUJIEUN/jpa-lecture/chap04-jpql-lecture-source/src/test/java/com/ohgiraffers/jpql/section01/simple/SimpleJPQLRepositoryTest.java
+++ 08_YUJIEUN/jpa-lecture/chap04-jpql-lecture-source/src/test/java/com/ohgiraffers/jpql/section01/simple/SimpleJPQLRepositoryTest.java
@@ -1,10 +1,11 @@
 package com.ohgiraffers.jpql.section01.simple;
 
-import jakarta.persistence.TypedQuery;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,26 +24,48 @@ class SimpleJPQLRepositoryTest {
 
     @DisplayName("Query를 이용한 단일행, 단일 컬럼 조회")
     @Test
-    void testSelectSingleMenuByQuery(){
+    void testSelectSingleMenuByQuery() {
         Object menuName = simpleJPQLRepository.selectSingleMenuByQuery();
         assertEquals("한우딸기국밥", menuName);
     }
 
-
     @DisplayName("TypedQuery를 이용한 단일행 조회")
     @Test
-    void testSelectSingleRowByTypedQuery(){
-        Menu menu = simpleJPQLRepository.selectSingleMenuByTypedQuery();
+    void testSelectSingleRowByTypedQuery() {
+        Menu menu = simpleJPQLRepository.selctSingleRowByTypedQuery();
         assertNotNull(menu);
         assertEquals("한우딸기국밥", menu.getMenuName());
     }
-    public Menu selectSingleRowByTypedQuery(){
-        String jpql ="SELECT m FROM Section01Menu as m WHERE m.menuCode = 8";
-        TypedQuery<Menu> query = entityManager.createQuery(jpql, );
-        Menu resultMenu = query.getSingleResult();
-        return resultMenu;
+
+    @DisplayName("TypedQuery를 이용한 다중행 조회")
+    @Test
+    void testSelectMultiByTypedQuery() {
+        List<Menu> menuList = simpleJPQLRepository.selectMultiRowByTypedQuery();
+        // menuList.forEach(System.out::println);
+        assertNotNull(menuList);
     }
 
+    @DisplayName("DISTINCT 연산자를 이용한 다중행 조회")
+    @Test
+    void testSelectUsingDistinct(){
+        List<Integer> categoryCodeList = simpleJPQLRepository.selectUsingDistinct();
+        System.out.println("categoryCodeList = " + categoryCodeList);
+        assertNotNull(categoryCodeList);
+    }
 
+    @DisplayName("IN 연산자 사용 조회 테스트")
+    @Test
+    void testSelectUsingIn(){
+        List<Menu> menuList = simpleJPQLRepository.selectUsingIn();
+        System.out.println("menuList = " + menuList);
+        assertNotNull(menuList);
+    }
 
+    @DisplayName("LIKE 연산자 사용 조회 테스트")
+    @Test
+    void testSelectUsingLike(){
+        List<Menu> menuList = simpleJPQLRepository.selectUsingLike();
+        System.out.println("menuList = " + menuList);
+        assertNotNull(menuList);
+    }
 }
