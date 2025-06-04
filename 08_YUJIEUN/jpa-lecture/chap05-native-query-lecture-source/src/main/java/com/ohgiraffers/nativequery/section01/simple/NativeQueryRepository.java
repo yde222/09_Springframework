ddb6e6b4db4991cdd5c71311dee272aaa2ec6bee
jieun <yudaeun222@gmail.com>
2025-06-04 17:10:28 +0900

    jpa

--- 08_YUJIEUN/jpa-lecture/chap05-native-query-lecture-source/src/main/java/com/ohgiraffers/nativequery/section01/simple/NativeQueryRepository.java
+++ 08_YUJIEUN/jpa-lecture/chap05-native-query-lecture-source/src/main/java/com/ohgiraffers/nativequery/section01/simple/NativeQueryRepository.java
@@ -13,23 +13,26 @@ public class NativeQueryRepository {
     @PersistenceContext
     private EntityManager entityManager;
 
-    public Menu nativeQueryByResultType(int menuCode) {
-        String query
-                = "SELECT" +
-                " menu_code, menu_name, menu_price, category_code, orderable_status" +
-                " FROM tbl_menu" +
-                " WHERE menu_code = ?";
+    public Menu nativeQueryByResultType(int menuCode){
+        /* Native Query 수행 결과를 엔터티에 매핑 시키기 위해서는
+        모든 컬럼이 처리 되어야만 매핑이 가능하다.
+        * */
+        String query = "SELECT menu_code, menu_name, menu_price, category_code, " +
+                "orderable_status FROM tbl_menu WHERE menu_code = ?";
         Query nativeQuery = entityManager.createNativeQuery(query, Menu.class)
                 .setParameter(1, menuCode);
         return (Menu) nativeQuery.getSingleResult();
     }
 
-    public List<Object[]> nativeQueryByNoResultType() {
+
+
+    public List<Object[]> nativeQueryByNoReultType(){
         String query = "SELECT menu_name, menu_price FROM tbl_menu";
-        return entityManager.createNativeQuery(query).getResultList();
+        Query nativeQuery = entityManager.createNativeQuery(query);
+        return nativeQuery.getResultList();
     }
 
-    public List<Object[]> nativeQueryByAutoMapping() {
+    public List<Object[]> nativeQueryByAutoMapping(){
         String query
                 = "SELECT a.category_code, a.category_name, a.ref_category_code," +
                 " COALESCE(v.menu_count, 0) menu_count" +
@@ -39,9 +42,31 @@ public class NativeQueryRepository {
                 " GROUP BY b.category_code) v ON (a.category_code = v.category_code)" +
                 " ORDER BY 1";
 
-        Query nativeQuery = entityManager.createNativeQuery(query, "categoryCountAutoMapping");
+        Query nativeQuery
+                = entityManager.createNativeQuery(query, "categoryCountAutoMapping");
         return nativeQuery.getResultList();
     }
+
+    public List<Object[]> nativeQueryByManualMapping(){
+        String query
+                = "SELECT a.category_code, a.category_name, a.ref_category_code," +
+                " COALESCE(v.menu_count, 0) menu_count" +
+                " FROM tbl_category a" +
+                " LEFT JOIN (SELECT COUNT(*) AS menu_count, b.category_code" +
+                " FROM tbl_menu b" +
+                " GROUP BY b.category_code) v ON (a.category_code = v.category_code)" +
+                " ORDER BY 1";
+        Query nativeQuery
+                = entityManager.createNativeQuery(query, "categoryCountManualMapping");
+        return nativeQuery.getResultList();
+
+    }
+
+
+
+
+
+
 
 
 }
--- 08_YUJIEUN/jpa-lecture/chap05-native-query-lecture-source/src/test/java/com/ohgiraffers/nativequery/section01/simple/NativeQueryRepositoryTest.java
+++ 08_YUJIEUN/jpa-lecture/chap05-native-query-lecture-source/src/test/java/com/ohgiraffers/nativequery/section01/simple/NativeQueryRepositoryTest.java
@@ -1,6 +1,5 @@
 package com.ohgiraffers.nativequery.section01.simple;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,63 +13,48 @@ import static org.junit.jupiter.api.Assertions.*;
 class NativeQueryRepositoryTest {
 
     @Autowired
-    private final NativeQueryRepository nativeQueryRepository;
+    private NativeQueryRepository nativeQueryRepository;
 
-    NativeQueryRepositoryTest(NativeQueryRepository nativeQueryRepository) {
-        this.nativeQueryRepository = nativeQueryRepository;
-    }
-
-    @DisplayName("결과 타입을 정의한 Native Query 사용 테스트")
+    @DisplayName("결과 타입을 정의한 Native Query 테스트")
     @Test
-    public void testNativeQueryByResultType() {
-        //given
+    void testNativeQueryByResultType() {
+
         int menuCode = 15;
-
-        //when
-        Menu foundMenu
-                = nativeQueryRepository.nativeQueryByResultType(menuCode);
-
-        //then
-        Assertions.assertNotNull(foundMenu);
-        System.out.println(foundMenu);
+        Menu foundMenu = nativeQueryRepository.nativeQueryByResultType(menuCode);
+        assertNotNull(foundMenu);
+        System.out.println("foundMenu = " + foundMenu);
     }
 
-    @DisplayName("결과 타입을 지정할 수 없는 Native Query 조회 테스트")
+    @DisplayName("결과 타입을 정의하지 않은 Native Query 테스트")
     @Test
-    public void testNativeQueryByNoResultType() {
-        //given
-        //when
-        List<Object[]> menuList
-                = nativeQueryRepository.nativeQueryByNoResultType();
-        //then
-        Assertions.assertNotNull(menuList);
-        menuList.forEach(
-                row -> {
-                    for(Object col : row) {
-                        System.out.print(col + " ");
-                    }
-                    System.out.println();
-                }
-        );
+    void testNativeQueryByNoResultType() {
+        List<Object[]> menuList = nativeQueryRepository.nativeQueryByNoReultType();
+        assertNotNull(menuList);
+        menuList.forEach(row -> {
+            for(Object column : row) System.out.print(column + " ");
+            System.out.println();
+        });
     }
 
-    @DisplayName("자동 결과 매핑을 사용한 Native Query 조회 테스트")
+    @DisplayName("자동결과 매핑을 사용한 Native Query 테스트")
     @Test
-    public void testNativeQueryByAutoMapping() {
-        //given
-        //when
-        List<Object[]> categoryList
-                = nativeQueryRepository.nativeQueryByAutoMapping();
+    void testNativeQueryByAutoMapping() {
+        List<Object[]> categoryList = nativeQueryRepository.nativeQueryByAutoMapping();
+        assertNotNull(categoryList);
+        categoryList.forEach(row -> {
+            for(Object column : row) System.out.print(column + " ");
+            System.out.println();
+        });
+    }
 
-        //then
-        Assertions.assertNotNull(categoryList);
-        categoryList.forEach(
-                row -> {
-                    for(Object col : row) {
-                        System.out.print(col + "/");
-                    }
-                    System.out.println();
-                }
-        );
+    @DisplayName("수동 결과 매핑을 사용한 Native Query 조회 테스트")
+    @Test
+    public void testNativeQueryByManualMapping(){
+        List<Object[]> categoryList = nativeQueryRepository.nativeQueryByManualMapping();
+        assertNotNull(categoryList);
+        categoryList.forEach(row -> {
+            for(Object column : row) System.out.print(column + "/ ");
+            System.out.println();
+        });
     }
 }
