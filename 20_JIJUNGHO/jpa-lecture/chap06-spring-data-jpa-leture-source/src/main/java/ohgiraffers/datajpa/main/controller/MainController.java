0e0dcfec6667f4a3cd23d3cc0ebd6717c1aac3c3
jungho <kiik52@naver.com>
2025-06-04 17:36:48 +0900

    chap06 - spring-data-jpa - 프로젝트 생성

---
+++ 20_JIJUNGHO/jpa-lecture/chap06-spring-data-jpa-leture-source/src/main/java/ohgiraffers/datajpa/main/controller/MainController.java
@@ -0,0 +1,14 @@
+package ohgiraffers.datajpa.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping(value = {"/", "/main"})
+    public String main() {
+        return "main";
+    }
+
+}
---
+++ 20_JIJUNGHO/jpa-lecture/chap06-spring-data-jpa-leture-source/src/main/java/ohgiraffers/datajpa/menu/dto/MenuDTO.java
@@ -0,0 +1,17 @@
+package ohgiraffers.datajpa.menu.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MenuDTO {
+
+    private int menuCode;
+    private String menuName;
+    private int menuPrice;
+    private int categoryCode;
+    private String orderableStatus;
+}
