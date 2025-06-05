83bb56d9b5f4e84b95dafa97c95a302ec9bcc89e
macacolabs <monkey.ohgiraffers@gmail.com>
2025-06-05 09:51:09 +0900

    feature: springdata jpa period 1 upload

--- 00_YUSEUNGJE/jpa-lecture/chap06-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/datajpa/config/BeanConfig.java
+++ 00_YUSEUNGJE/jpa-lecture/chap06-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/datajpa/config/BeanConfig.java
@@ -1,7 +1,26 @@
 package com.ohgiraffers.datajpa.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+/* ModelMapper : 런타임 시 객체 간의 매핑을 자동화해주는 라이브러리로, 기본 설정만으로도 단순한 변환을 수행할 수 있음
+ *    - 코드 작성량 감소 및 설정을 통해 다양한 케이스를 지원, 복잡한 객체 구조의 변환에도 유연하게 대처 가능
+ *    - 런타임에 Reflection을 사용하므로 성능 오버헤드가 있을 수 있음, 복잡한 매핑 규칙은 추가 설정이 필요
+ * */
 
 @Configuration
 public class BeanConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        /* setter 메소드 미사용 시 ModelMapper가 private 필드에 접근가능하도록 설정 */
+        modelMapper.getConfiguration()
+                .setFieldAccessLevel(
+                        org.modelmapper.config.Configuration.AccessLevel.PRIVATE
+                )
+                .setFieldMatchingEnabled(true);
+        return modelMapper;
+    }
 }
--- 00_YUSEUNGJE/jpa-lecture/chap06-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/datajpa/menu/service/MenuService.java
+++ 00_YUSEUNGJE/jpa-lecture/chap06-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/datajpa/menu/service/MenuService.java
@@ -1,7 +1,41 @@
 package com.ohgiraffers.datajpa.menu.service;
 
+import com.ohgiraffers.datajpa.menu.dto.MenuDTO;
+import com.ohgiraffers.datajpa.menu.entity.Menu;
+import com.ohgiraffers.datajpa.menu.respository.MenuRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MenuService {
+
+    private final MenuRepository menuRepository;
+    private final ModelMapper modelMapper;
+
+    /* DTO와 Entity의 분리
+     * DTO(Data Transfer Object)는 프레젠테이션 계층과의 데이터 교환을 위한 객체로,
+     * 도메인 로직을 담고 있는 Entity와 분리하여 사용함으로써 보안, 성능, 유지보수 측면의 이점을 얻을 수 있다.
+     * 1. 보안 : 민감 데이터/과도 데이터 노출 방지, 과도한 바인딩 공격 방지
+     * 2. 성능 : 데이터 전송 최적화, 지연 로딩 문제 완화, 쿼리 최적화
+     * 3. 유지 보수 : 명확한 계층 분리(비즈니스 로직 수정이나 도메인 모델 변경이 클라이언트와의 API 계약에 영향X)
+     *               도메인 로직의 캡슐화(Entity 관련 비즈니스 로직은 외부에 노출 X, 클라이언트는 DTO만 이용)
+     * */
+
+    /* DTO - Entity의 변환 방법
+     * 1. 수동 매핑 메서드
+     * 2. Spring BeanUtils.copyProperties()
+     * 3. ModelMapper 라이브러리
+     * 4. MapStruct 라이브러리
+     * 5. Spring Data JPA Projection */
+
+    /* 1. findById */
+    public MenuDTO findMenuByMenuCode(int menuCode) {
+
+        Menu menu = menuRepository.findById(menuCode)
+                .orElseThrow(IllegalArgumentException::new);
+        //return MenuDTO.changeMenuDto(menu);
+        return modelMapper.map(menu, MenuDTO.class);
+    }
 }
