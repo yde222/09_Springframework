0cf074548df2ffd4970ee2fed998c6c53311f737
jieun <yudaeun222@gmail.com>
2025-06-05 11:03:53 +0900

    add

--- 08_YUJIEUN/jpa-lecture/chap06-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/datajpa/menu/respository/MenuRepository.java
+++ 08_YUJIEUN/jpa-lecture/chap06-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/datajpa/menu/respository/MenuRepository.java
@@ -3,5 +3,8 @@ package com.ohgiraffers.datajpa.menu.respository;
 import com.ohgiraffers.datajpa.menu.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MenuRepository extends JpaRepository<Menu, Integer> {
+    List<Menu> mu
 }
