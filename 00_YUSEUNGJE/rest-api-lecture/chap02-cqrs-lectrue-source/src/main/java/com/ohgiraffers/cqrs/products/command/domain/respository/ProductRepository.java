f66c23d2faeb9ed1f479f9371568054841286529
macacolabs <monkey.ohgiraffers@gmail.com>
2025-06-09 12:28:28 +0900

    feature: create source

--- 00_YUSEUNGJE/rest-api-lecture/chap02-cqrs-lectrue-source/src/main/java/com/ohgiraffers/cqrs/products/command/domain/respository/ProductRepository.java
+++ 00_YUSEUNGJE/rest-api-lecture/chap02-cqrs-lectrue-source/src/main/java/com/ohgiraffers/cqrs/products/command/domain/respository/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.ohgiraffers.cqrs.products.command.domain.respository;
 
+import com.ohgiraffers.cqrs.products.command.domain.aggregate.Product;
+
 public interface ProductRepository {
+    Product save(Product newProduct);
 }
